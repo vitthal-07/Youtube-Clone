953677c8e213127212e21d34bd04f554edd67cb8
vitthal-07 <vitthalbiradar50@gmail.com>
2024-06-09 16:08:39 +0530

    updated iframe

--- src/Components/Watch/VideoPlayer.jsx
+++ src/Components/Watch/VideoPlayer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const VideoPlayer = ({ videoId }) => (
     <iframe
         className='rounded-[16px] w-full h-[275px] md:h-[575px]'
-        src={`https://www.youtube.com/embed/${videoId}?autoplay=1&si=y0ELnEy0ubWFX5rc`}
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1&si=JUXCgZdiUb38sCNN`}
         title='YouTube video player'
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-        referrerPolicy='strict-origin-when-cross-origin'
+        referrerpolicy='strict-origin-when-cross-origin'
     ></iframe>
 );
 
